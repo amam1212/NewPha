ef668922ab52dc55ac47badd9095b046df8fba88
paruj <paruj_lertjiranuwat@cmu.ac.th>
2016-05-23 18:58:28 +0700

    Initial

---
+++ WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using WebApplication2.Models.Repository;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductRepository rep = new ProductRepository();
+        // GET: Product
+        public ActionResult Index()
+        {
+           
+            return View(rep.GetAllProduct());
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                var name = collection["Name"];
+                var price = collection["Price"];
+                double p = double.Parse(price);
+                Product product = new Product();
+                product.Name = name;
+                product.Price = p;
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
